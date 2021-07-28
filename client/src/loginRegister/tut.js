36a1a3830a718c2baa0801c97961cfda468ac548
JAK-CAL <big01ad@kaist.ac.kr>
2021-07-28 07:40:36 +0000

    a

--- client/src/loginRegister/tut.js
+++ client/src/loginRegister/tut.js
@@ -8,12 +8,24 @@ import "./tut.css"
 var PointCalibrate = 0;
 var CalibrationPoints={};
 
-const tut = (props) => {
+const Tut = (props) => {
+    const [modalopen,setmodal] = useState(true);
+
 
     const appStyle = {
         height: '800px',
         display: 'flex'
       };
+
+      const formStyle = {
+        margin: 'auto',
+        padding: '10px',
+        border: '1px solid #c9c9c9',
+        borderRadius: '5px',
+        background: '#f5f5f5',
+        width: '300px',
+          display: 'block'
+    };
 
     function calculatePrecision(past50Array) {
       
@@ -260,8 +272,9 @@ const tut = (props) => {
     }
 
     return (
-      
+      <div>
         <div style={appStyle}>
+          
             <div onClick={saveGaze} className="ml-auto">
                 <button class="btn btn-light">Save</button>
             </div>
@@ -277,37 +290,89 @@ const tut = (props) => {
             <div onClick={recordGaze}>
                 <button className="btn btn-light">Start</button>
             </div>
-            <div className="calibrationDiv">
-              <input type="button" className="Calibration" id="Pt1"></input>
-              <input type="button" className="Calibration" id="Pt2"></input>
-              <input type="button" className="Calibration" id="Pt3"></input>
-              <input type="button" className="Calibration" id="Pt4"></input>
-              <input type="button" className="Calibration" id="Pt5"></input>
-              <input type="button" className="Calibration" id="Pt6"></input>
-              <input type="button" className="Calibration" id="Pt7"></input>
-              <input type="button" className="Calibration" id="Pt8"></input>
-              <input type="button" className="Calibration" id="Pt9"></input>
-            </div>
+           
+            
 
-            <div id="helpModal" className="modal fade" role="dialog">
-              <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-body">
-                  <img src="media/example/calibration.png" width="100%" height="100%" alt="webgazer demo instructions"></img>
+        </div>
+        <>
+        {modalopen ? (  
+
+         ////만약 isopen(this.state.isModalOpen)이 true일때 코드를 실행 false면  null
+        /// <div onClick={close}> 로그인창 말고 회색 바탕을 누를시 모달이 꺼지게 만듬
+	      ///<span className="close" onClick={close}>&times;</span> x버튼 누를시 꺼짐
+        ////<div className="modalContents" onClick={isOpen}> 로그인 화면은 버튼 클릭해서 들어오면
+         /// true인 상태로 있어서 화면이 안꺼진다.
+      
+          <div className="modal">
+            <div onClick={close}>
+              <div className="loginModal">
+                <span className="close" onClick={close}>
+                  &times;
+                </span>
+                <div className="modalContents" onClick={isOpen}>
+                  <img
+                    className="signinIcon"
+                    src="/Images/SignIn/signinIcon.png"
+                  />
+                  <input
+                    name="email"
+                    className="loginId"
+                    type="text"
+                    placeholder="아이디"
+                    onChange={this.loginHandler}
+                  />
+                  <input
+                    name="password"
+                    className="loginPw"
+                    type="password"
+                    placeholder="비밀번호"
+                    onChange={this.loginHandler}
+                  />
+                  <div className="loginMid">
+                    <label className="autoLogin" for="hint">
+                      {" "}
+                      <input type="checkbox" id="hint" /> 로그인 유지하기
+                    </label>
+                    <div className="autoLogin">아이디/비밀번호 찾기</div>
+                  </div>
+                  <button className="loginBtn" onClick={this.loginClickHandler}>
+                    {" "}
+                    로그인{" "}
+                  </button>
+                  <div className="socialBox">
+                    <div className="kakao">
+                      <img
+                        className="kakaoLogo"
+                        src="/Images/SignIn/kakao.png"
+                      />
+                      <div className="kakaoText">카카오 계정으로 신규가입</div>
+                    </div>
+                    <div className="facebook">
+                      <img
+                        className="facebookLogo"
+                        src="/Images/SignIn/facebook.png"
+                      />
+                      <div className="facebookText">
+                        페이스북 계정으로 신규가입
+                      </div>
+                    </div>
+                  </div>
+                  <div className="loginEnd">
+                    <div className="loginLine">
+                      회원이 아니신가요? <Link to="/signup">회원가입</Link>
+                    </div>
+                    <div className="noUser">비회원 주문 조회</div>
+                  </div>
                 </div>
-              <div className="modal-footer">
-                <button id="closeBtn" type="button" className="btn btn-default" data-dismiss="modal">Close & load saved model </button>
-                <button type="button" id='start_calibration' className="btn btn-primary" data-dismiss="modal" onClick={webgazer.resume}>Calibrate</button>
               </div>
             </div>
-
           </div>
-        </div>
-        </div>
-        
+        ) : null}
+      </>
+      </div>
     );
 }
 
 
 //onChange={this.handleChange} onChange={this.handleChange}
-export default tut;
+export default Tut;
