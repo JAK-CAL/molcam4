224d4bbb128a3f6c6d204e1e02071d18e5ace301
jungjaechoi <www898975@gmail.com>
2021-07-29 07:50:24 +0000

    add avg

--- client/src/postboard/average.js
+++ client/src/postboard/average.js
@@ -10,7 +10,8 @@ const Average= () => {
     const [post, setpost] = useState({
         old:0,
         bed:"",
-        playg:'',
+        soil:"",
+        grass:'',
         futsal:"",
         basket:"",
         tenis:"",
@@ -26,11 +27,14 @@ const Average= () => {
 
 
     let [avg_old,avg_vac,avg_pc,avg_trans,avg_taste,avg_PXdis,avg_health] = [0,0,0,0,0,0,0,0];
-    const {old,bed,playg,futsal,basket,tenis,vaca,health,PC,trans,taste,PXdis,etc} = post;
+    let [cnt_bed,cnt_soil,cnt_grass,cnt_futsal,cnt_tenis] = [0,0,0,0,0];
+    const {old,bed,soil,grass,futsal,basket,tenis,vaca,health,PC,trans,taste,PXdis,etc} = post;
 
     useEffect(()=> {
         handleLoadAll();
     },[]);
+
+
 
     const makeAvg = (name,value) => {
         return (
@@ -46,26 +50,40 @@ const Average= () => {
         axios
             .post("http://192.249.18.153:80/user/loadAll")
             .then(returnData=>{
+                console.log(returnData)
                 if(returnData.status === 200){
-                    
-
                     for(let i =0; i<returnData.data.length; i++){
+                        if (returnData.data[i].bed === "bed") cnt_bed++;
+                        if (returnData.data[i].grass !== "") cnt_grass++;
+                        if (returnData.data[i].soil !== "") cnt_soil++;
+                        if (returnData.data[i].futsal !== "") cnt_futsal++;
+                        if (returnData.data[i].tenis !== "") cnt_tenis++;
+
                         avg_old += parseInt(returnData.data[i].old);
-                        avg_vac += parseInt(returnData.data[i].vaca);
-                        avg_pc += parseInt(returnData.data[i].PC);
-                        avg_trans += parseInt(returnData.data[i].trans);
-                        avg_taste += parseInt(returnData.data[i].taste);
-                        avg_PXdis += parseInt(returnData.data[i].PXdis);
-                        avg_health += parseInt(returnData.data[i].health);
-                        console.log(avg_vac+" " +returnData.data[i])
+                        //avg_vac += parseInt(returnData.data[i].vaca);
+                        //vac data가 없어서 오류나는 중
+                        if (returnData.data[i].PC !== "") avg_pc += parseInt(returnData.data[i].PC);
+                        if (returnData.data[i].trans !== "") avg_trans += parseInt(returnData.data[i].trans);
+                        if (returnData.data[i].taste !== "") avg_taste += parseInt(returnData.data[i].taste);
+                        if (returnData.data[i].PXdis !== "") avg_PXdis += parseInt(returnData.data[i].PXdis);
+                        if (returnData.data[i].health !== "") avg_health += parseInt(returnData.data[i].health);
+                        
                     }
+
                     avg_old /= returnData.data.length;
-                    avg_vac /= returnData.data.length;
+                    //avg_vac /= returnData.data.length;
                     avg_pc /= returnData.data.length;
                     avg_trans /= returnData.data.length;
                     avg_taste /= returnData.data.length;
                     avg_PXdis /= returnData.data.length;
                     avg_health /= returnData.data.length;
+
+                    cnt_bed = cnt_bed/returnData.data.length > 0.5 ? '침대' : '침상'
+                    cnt_tenis = cnt_tenis/returnData.data.length > 0.5 ? '있음' : '없음'
+                    cnt_soil = cnt_soil/returnData.data.length > 0.5 ? '있음' : '없음'
+                    cnt_grass = cnt_grass/returnData.data.length > 0.5 ? '있음' : '없음'
+                    cnt_futsal = cnt_futsal/returnData.data.length > 0.5 ? '있음' : '없음'
+
                     setpost({
                         ...post,
                         "old":avg_old,
@@ -75,6 +93,11 @@ const Average= () => {
                         "taste":avg_taste,
                         "PXdis":avg_PXdis,
                         "health":avg_health,
+                        "bed":cnt_bed,
+                        "tenis":cnt_tenis,
+                        "soil":cnt_soil,
+                        "grass":cnt_grass,
+                        "futsal":cnt_futsal,
                     })
                     //alert('load');
                     console.log(returnData)
@@ -93,6 +116,11 @@ const Average= () => {
             {makeAvg("교통",trans)}
             {makeAvg("밥",taste)}
             {makeAvg("PX",PXdis)}
+            {makeAvg("풋살장",futsal)}
+            {makeAvg("테니스장",tenis)}
+            {makeAvg("잔디 운동장",grass)}
+            {makeAvg("흙 운동장",soil)}
+            {makeAvg("침대/침상:",bed)}
             <button onClick = {handleLoadAll}>불러오기</button>
         </div>
         
